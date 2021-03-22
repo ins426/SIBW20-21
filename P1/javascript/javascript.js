ea00cea2545e2bd678fe272b1358780309e913e2
Ines Nieto <insherondale26@gmail.com>
2021-03-22 02:08:30 +0100

    P1 arreglada y P2 empezada

---
+++ P1/javascript/javascript.js
@@ -0,0 +1,7 @@
+function abrirComentarios(){
+    document.getElementById("comentarios").style.width = "250px";
+}
+
+function cerrarComentarios(){
+    document.getElementById("comentarios").style.width = "0px";
+}
